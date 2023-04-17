a94f5bfe042f89db88e239409bdd12913d99894b
mokevip <652831080@qq.com>
2023-04-17 17:45:19 +0800

    优化build时路径问题

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ module.exports = function rollupRewrite(options) {
     return {
         name: 'vite-plugin-rewrite',
         enforce: 'pre', // 标注必须在vite核心插件之前介入
-        resolveId(id) {
+        resolveId(id, pat) {
+            let dir2 = ''
             if (include && !include.test(id)) {
                 return null;
             }
             if (exclude && exclude.test(id)) {
                 return null;
             }
-            const modifiedPath = path.join(dir, id.replace(/(\..+?$)/g, `_${sign}$1`))
+            if (/^\./.test(id)) {
+                dir2 = pat.replace(/([\/\\].+)([\/\\].+?\..+?)$/, `$1${path.sep}`)
+                id = path.join(dir2, id)
+            }
+            if (/^[\/\\]/.test(id)) {
+                id = path.join(dir, id)
+            }
+            const modifiedPath = id.replace(/(\..+?$)/g, `_${sign}$1`)
             if (existsSync(modifiedPath)) {
+                console.log('modifiedPath', modifiedPath)
                 return modifiedPath
             }
         },
