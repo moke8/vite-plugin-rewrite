8ec782e79ebbf31616555c7e3544e98a45953daf
mokevip <652831080@qq.com>
2023-04-17 19:43:04 +0800

    解决build编译问题

--- index.js
+++ index.js
@@ -7,25 +7,19 @@ module.exports = function rollupRewrite(options) {
     return {
         name: 'vite-plugin-rewrite',
         enforce: 'pre', // 标注必须在vite核心插件之前介入
-        resolveId(id, pat) {
-            let dir2 = ''
+        load(id) {
             if (include && !include.test(id)) {
                 return null;
             }
             if (exclude && exclude.test(id)) {
                 return null;
             }
-            if (/^\./.test(id)) {
-                dir2 = pat.replace(/([\/\\].+)([\/\\].+?\..+?)$/, `$1${path.sep}`)
-                id = path.join(dir2, id)
-            }
-            if (/^[\/\\]/.test(id)) {
-                id = path.join(dir, id)
-            }
             const modifiedPath = id.replace(/(\..+?$)/g, `_${sign}$1`)
             if (existsSync(modifiedPath)) {
-                console.log('modifiedPath', modifiedPath)
-                return modifiedPath
+                const result = readFileSync(modifiedPath).toString()
+                return {
+                    code: result
+                }
             }
         },
     };
